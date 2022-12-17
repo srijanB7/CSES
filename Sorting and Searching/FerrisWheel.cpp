19019aa8e9816a20ceca8297b640e10784a6d858
srijanbiswas7 <srijanbiswas955@gmail.com>
2022-12-17 23:23:23 +0530

    3rd prob

--- Sorting and Searching/FerrisWheel.cpp
+++ Sorting and Searching/FerrisWheel.cpp
@@ -1,7 +1,26 @@
 #include <bits/stdc++.h>
 
+using ll = long long;
 using namespace std;
 
 int main() {
-    
+    int n, p;
+    cin >> n >> p;
+    vector<int> v(n);
+    for(int &i : v) 
+        cin >> i;
+    sort(v.begin(), v.end());
+    int cnt = 0;
+    int i = 0, j = n-1;
+    while(i <= j) {
+        if(v[i] + v[j] <= p) {
+            cnt++;
+            j--;
+            i++;
+        } else {
+            cnt++;
+            j--;
+        }
+    }
+    cout << cnt << endl;
 }
